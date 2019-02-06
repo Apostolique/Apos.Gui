ca0c3da0a210424ec377f4ec7c2799a1f9cf58ad
Jean-David Moisan <vynecompiler@gmail.com>
2019-02-06 12:58:58 -0500

    Added better defaults

--- Source/Default.cs
+++ Source/Default.cs
@@ -1,6 +1,7 @@
 using System;
 using Microsoft.Xna.Framework.Input;
 using Apos.Input;
+using Microsoft.Xna.Framework;
 
 namespace Apos.Gui {
     /// <summary>
@@ -11,19 +12,61 @@ namespace Apos.Gui {
         public static Func<Component, bool> ConditionHoverMouse = (Component b) => b.IsInsideClip(GuiHelper.MouseToUI());
         public static Func<Component, bool> ConditionGotHovered = (Component b) => !b.OldIsHovered && b.IsHovered;
         public static Func<Component, bool> ConditionInteraction = (Component b) =>
-                b.HasFocus && (ButtonReleased(InputHelper.OldGamePad[0].Buttons.A, InputHelper.NewGamePad[0].Buttons.A) ||
-                               KeyReleased(Keys.Space) || KeyReleased(Keys.Enter)) ||
-                b.IsHovered && ButtonReleased(InputHelper.OldMouse.LeftButton, InputHelper.NewMouse.LeftButton);
-
-        public static Func<Keys, bool> KeyReleased = (key) =>
-            InputHelper.OldKeyboard.IsKeyDown(key) && InputHelper.NewKeyboard.IsKeyUp(key);
-        public static Func<ButtonState, ButtonState, bool> ButtonReleased = (oldButtonState, newButtonState) =>
-            oldButtonState == ButtonState.Pressed && newButtonState == ButtonState.Released;
+            b.HasFocus && (buttonReleased(s => s[0].Buttons.A) ||
+                           buttonReleased(Keys.Space) || buttonReleased(Keys.Enter)) ||
+            b.IsHovered && buttonReleased(s => s.LeftButton);
+        public static Func<bool> ConditionPreviousFocus = () =>
+            InputHelper.OldGamePad[0].ThumbSticks.Left.Y <= 0 && InputHelper.NewGamePad[0].ThumbSticks.Left.Y > 0 ||
+            buttonReleased(Keys.Up);
+        public static Func<bool> ConditionNextFocus = () =>
+            InputHelper.OldGamePad[0].ThumbSticks.Left.Y >= 0 && InputHelper.NewGamePad[0].ThumbSticks.Left.Y < 0 ||
+            buttonReleased(Keys.Down);
+        public static Func<bool> ConditionBackFocus = () =>
+            buttonReleased(s => s[0].Buttons.B) ||
+            buttonReleased(Keys.Escape);
+        public static Func<Component, bool> ConsumeCondition = (Component c) => true;
 
         // Group: Public Functions
+        public static Component GiveHover(Component c) {
+            c.AddHoverCondition(ConditionHoverMouse);
 
-        // Group: Private Variables
+            return c;
+        }
+        public static Component CreateButton(string text, Func<Component, bool> operation, Action<Component> grabFocus) {
+            Label l = new Label(text);
+            l.ActiveColor = Color.White;
+            l.NormalColor = new Color(150, 150, 150);
+
+            return CreateButton(l, operation, grabFocus);
+        }
+        public static Component CreateButtonDynamic(Func<string> text, Func<Component, bool> operation, Action<Component> grabFocus) {
+            LabelDynamic l = new LabelDynamic(text);
+            l.ActiveColor = Color.White;
+            l.NormalColor = new Color(150, 150, 150);
+
+            return CreateButton(l, operation, grabFocus);
+        }
+        public static Component CreateButton(Component c, Func<Component, bool> operation, Action<Component> grabFocus) {
+            Border border = new Border(c, 20, 20, 20, 20);
+            Button b = new Button(border);
+            b.ShowBox = false;
+            b.GrabFocus = grabFocus;
+            b.AddHoverCondition(ConditionHoverMouse);
+            b.AddAction(ConditionInteraction, operation);
+            b.AddAction(ConditionGotHovered, ConsumeCondition);
+
+            return b;
+        }
 
         // Group: Private Functions
+        private static bool buttonReleased(Keys key) {
+            return ActionKeyboard.Released(key);
+        }
+        private static bool buttonReleased(Func<MouseState, ButtonState> button) {
+            return ActionMouse.Released(button);
+        }
+        private static bool buttonReleased(Func<GamePadState[], ButtonState> button) {
+            return ActionGamePad.Released(button);
+        }
     }
 }
