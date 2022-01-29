348d2bebcf04b49b89ef6cfe9fff075b50435f45
Jean-David Moisan <vynecompiler@gmail.com>
2022-01-29 08:10:42 -0500

    Label cache mechanic

--- Source/Label.cs
+++ Source/Label.cs
@@ -6,44 +6,55 @@ using Microsoft.Xna.Framework;
 namespace Apos.Gui {
     public class Label : Component {
         public Label(int id, string text, int fontSize, Color c) : base(id) {
-            // TODO: Optimize cache, no need to cache twice. Will need to also figure out updating from outside.
             Text = text;
-            Color = c;
             FontSize = fontSize;
+            Color = c;
         }
 
         public string Text {
-            get => _text;
+            get => _nextText;
             set {
-                if (value != _text) {
-                    _text = value;
-                    _cachedSize = GuiHelper.MeasureString(_text, FontSize);
+                if (value != _nextText) {
+                    _nextText = value;
+                    _isUpdateText = true;
                 }
             }
         }
         public int Padding { get; set; } = 10;
         public Color Color { get; set; }
         public int FontSize {
-            get => _fontSize;
+            get => _nextSize;
             set {
-                if (value != _fontSize) {
-                    _fontSize = value;
-                    _cachedSize = GuiHelper.MeasureString(_text, FontSize);
+                if (value != _nextSize) {
+                    _nextSize = value;
+                    _isUpdateText = true;
                 }
             }
         }
 
         public override void UpdatePrefSize(GameTime gameTime) {
+            if (_isUpdateText) {
+                Cache();
+            }
+
             PrefWidth = _cachedSize.X + Padding * 2;
             PrefHeight = _cachedSize.Y + Padding * 2;
         }
         public override void Draw(GameTime gameTime) {
             GuiHelper.PushScissor(Clip);
 
-            var font = GuiHelper.GetFont(FontSize);
-            GuiHelper.SpriteBatch.DrawString(font, Text, XY + new Vector2(Padding), Color, GuiHelper.FontScale);
+            var font = GuiHelper.GetFont(_fontSize);
+            // TODO: Hover color?
+            GuiHelper.SpriteBatch.DrawString(font, _text, XY + new Vector2(Padding), Color, GuiHelper.FontScale);
 
             GuiHelper.PopScissor();
+        }
+
+        protected void Cache() {
+            _text = _nextText;
+            _fontSize = _nextSize;
+            _cachedSize = GuiHelper.MeasureString(_text, _fontSize);
+            _isUpdateText = false;
         }
 
         public static Label Put(string text, int fontSize = 30, Color? color = null, [CallerLineNumber] int id = 0, bool isAbsoluteId = false) {
@@ -79,5 +90,9 @@ namespace Apos.Gui {
         protected string _text;
         protected int _fontSize;
         protected Vector2 _cachedSize;
+
+        protected bool _isUpdateText = false;
+        protected string _nextText = "";
+        protected int _nextSize;
     }
 }
